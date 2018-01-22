b5378bebf87a71c534b7243b77af7f4616a17e6e
Elliot Still <cl04ker@gmail.com>
2018-01-22 01:14:38 +0000

    Fixed ending message on setup.sh

--- setup.sh
+++ setup.sh
@@ -45,4 +45,4 @@ make -j$(nproc)
 make install
 chown -R $(logname): ../HTMLCOIN
 
-echo -e "\e[1m\e[5m\e[92mSetup complete! Now run ./htmlcoin-multiminer-start.sh\e[0m"
+echo -e "\e[1m\e[5m\e[92mSetup complete! Now run ./start.sh\e[0m"
